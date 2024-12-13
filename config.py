71c322de03a38ffa05e0bf7e324da55432da2909
Alexander B <bas2784@gmail.com>
2024-12-13 09:27:25 +0300

    create new structure

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-API_KEY = '7483592291:AAE82DXPrZV0d-U-dFktEaIzyJ2ypxZsk_A'
-
 priceM = 1500
 priceL = 2000
 priceXL = 3000
--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-connection = sqlite3.connect('data.db')
+connection = sqlite3.connect('old/data.db')
 cursor = connection.cursor()
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS Users (
